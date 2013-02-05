c0cb32a0839cee4038634abc1e457b5b87ca38a2
Łukasz Wiecheć <lwiechec@o2.pl>
2013-02-05 15:40:45 +0100

    putting malabar-mode initialization to a separate el-get init file

---
+++ etc/emacs.d/el-get-init-files/init-malabar-mode.el
@@ -0,0 +1,11 @@
+(add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+
+; optional: simulate constant compile-after-save (as in Eclipse)
+;(add-hook 'malabar-mode-hook
+;     (lambda ()
+;       (add-hook 'after-save-hook 'malabar-compile-file-silently
+;                  nil t)))
+
+(add-hook 'malabar-mode-hook  (lambda()
+    (local-set-key [(f11)] 'javadoc-lookup)
+    (local-set-key [(shift f11)] 'javadoc-help)))
