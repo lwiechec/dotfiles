b30fa5ac31846aec698c995f2132fb1ca8a499f6
Łukasz Wiecheć <lwiechec@o2.pl>
2013-02-07 15:22:43 +0100

    further cleanups in .emacs file after switching to el-get
    initialization code moved from .emacs to init files. Also - fixing
    problem with dired-single.

---
+++ etc/emacs.d/el-get-init-files/init-auto-complete.el
@@ -0,0 +1,7 @@
+(require 'auto-complete)
+(setq-default ac-sources (add-to-list 'ac-sources 'ac-source-dictionary))
+(global-auto-complete-mode t)
+; Start auto-completion after 2 characters of a word
+(setq ac-auto-start 2)
+; case sensitivity is important when finding matches
+(setq ac-ignore-case nil)
---
+++ etc/emacs.d/el-get-init-files/init-dired-single.el
@@ -0,0 +1,14 @@
+;; use single buffer for dired to avoid too many of them
+(defun my-dired-init ()
+  (define-key dired-mode-map [return] 'dired-single-buffer)
+  (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
+  (define-key dired-mode-map "^"
+    (function
+     (lambda nil (interactive) (dired-single-buffer "..")))))
+
+;; if dired's already loaded, then the keymap will be bound
+(if (boundp 'dired-mode-map)
+    ;; we're good to go; just add our bindings
+    (my-dired-init)
+  ;; it's not loaded yet, so add our bindings to the load-hook
+  (add-hook 'dired-load-hook 'my-dired-init))
