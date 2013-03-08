20f38f4d62e88dc612523171be0e9313a046dd52
Łukasz Wiecheć <lwiechec@o2.pl>
2013-03-08 11:46:37 +0100

    adding shell-pop for quick access to the shell

---
+++ etc/emacs.d/el-get-init-files/init-shell-pop.el
@@ -0,0 +1,7 @@
+; shell-pop - easily into and out of the shell using one key
+(shell-pop-set-universal-key (kbd "C-`"))
+(shell-pop-set-internal-mode "shell")
+(shell-pop-set-internal-mode-shell "/bin/bash")
+(shell-pop-set-window-height 50)
+(shell-pop-set-window-position "bottom")
+(shell-pop-set-default-directory "/home/luke/work")
