a66682bd427a7b864315a3b05e37a34367c3c666
Łukasz Wiecheć <lwiechec@o2.pl>
2013-03-22 16:55:45 +0100

    adding and setting up javadoc-lookup
    decided to use w3m as a browser so we can work from emacs

---
+++ etc/emacs.d/el-get-init-files/init-javadoc-lookup.el
@@ -0,0 +1,8 @@
+; add local copies of Java API
+(javadoc-add-roots "~/work/doc/jdk-6u30-apidocs")
+
+; add other packages using Maven notation: [groupId artifactId version]
+(javadoc-add-artifacts [commons-configuration commons-configuration "1.6" ])
+
+; use w3m to browse
+(setq browse-url-browser-function 'w3m-browse-url)
