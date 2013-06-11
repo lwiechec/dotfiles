a95dbb8c0d95d9add119a4046cff65a9875a633a
Łukasz Wiecheć <lwiechec@o2.pl>
2013-06-11 16:22:53 +0200

    fixes to malabar-mode
    hopefully it will make work with java files easier. Defined an alternate
    version of 'run current test via Maven'; also after finding a very
    interesting and inspiring article on emacs wiki added automatic closing
    of the compilation window.

--- etc/emacs.d/el-get-init-files/init-malabar-mode.el
+++ etc/emacs.d/el-get-init-files/init-malabar-mode.el
@@ -6,7 +6,28 @@
 ;       (add-hook 'after-save-hook 'malabar-compile-file-silently
 ;                  nil t)))
 
+; close compilation window if everything went well
+; inspired by this discussion: http://www.emacswiki.org/emacs/ModeCompile
+(defun compile-autoclose (buffer string)
+  (with-selected-window (get-buffer-window buffer)
+    (goto-char (point-max)))
+  (cond ((string-match "finished" string)
+	 (message "Build probably successful: closing window in 5 secs...")
+	 (run-with-timer 5 nil
+			 'delete-window
+			 (get-buffer-window buffer t)))
+	(t
+	 (message "Compilation exited abnormally: %s" string))))
+(setq compilation-finish-functions 'compile-autoclose)
+
+; as the stock 'Run test via maven from malabar' does not work for me, here's a workaround
+(defun my-malabar-run-test ()
+  (interactive)
+  "runs Maven test for current buffer. Does not perform check if the test is actually a test."
+  (malabar-run-maven-command (concat "-Dtest=" (car (split-string (buffer-name (current-buffer)) ".java")) " test")))
+
 (add-hook 'malabar-mode-hook  (lambda()
     (local-set-key [(f11)] 'javadoc-lookup)
     (local-set-key [(shift f11)] 'javadoc-help)
-    (local-set-key (kbd "C-c C-v C-n") 'malabar-visit-corresponding-test)))
+    (local-set-key (kbd "C-c C-v C-n") 'malabar-visit-corresponding-test)
+    (local-set-key (kbd "C-c C-v T") 'my-malabar-run-test)))
