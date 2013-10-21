c6d0136b4604e49d5d6070a1648113115b2c35d3
Łukasz Wiecheć <lwiechec@o2.pl>
2013-10-21 15:31:40 +0200

    removing el-get
    it will be installed automatically- no need to have it in the repo

--- etc/emacs.d/el-get/el-get/recipes/emacs-pmd.el
+++
@@ -1,7 +0,0 @@
-(:name emacs-pmd
-       :website "http://yrk.nfshost.com/projects/emacs-pmd/"
-       :description "Emacs interface to PMD, offline Java code anaylizer"
-       :type git
-       :url "http://yrk.nfshost.com/repos/emacs-pmd.git/"
-       ;;:build ("cd docs; makeinfo")
-       :features emacs-pmd)
