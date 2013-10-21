a556ccc1666ebcc6b618422ba730ef48bc61c5ae
Łukasz Wiecheć <lwiechec@o2.pl>
2013-10-21 17:00:22 +0200

    adding more init files

---
+++ etc/emacs.d/el-get-init-files/init-skeleton-complete.el
@@ -0,0 +1,2 @@
+(require 'skeleton-complete)
+(skeleton-complete-global-mode 1)
---
+++ etc/emacs.d/el-get-init-files/init-slime.el
@@ -0,0 +1,3 @@
+(slime-setup '(slime-banner slime-fancy))
+; i'm using sbcl for my lisp
+(setq inferior-lisp-program "sbcl")
