233728cb249beb6eb3e26bbfd901045bc8ec7f74
Łukasz Wiecheć <lwiechec@o2.pl>
2013-10-21 17:00:03 +0200

    updating to the latest version

--- etc/emacs.d/el-get-init-files/init-eclim.el
+++ etc/emacs.d/el-get-init-files/init-eclim.el
@@ -1,7 +1,15 @@
-(global-eclim-mode)
+; setting where Eclipse and eclim is installed
 (custom-set-variables
  '(eclim-eclipse-dirs '("~/work/tools/eclipse"))
  '(eclim-executable "~/work/tools/eclipse/eclim"))
+
+(setq eclimd-executable "~/work/tools/eclipse/eclimd")
+
+; start eclimd in the buffer
+(start-process "eclimd" "*eclimd*" eclimd-executable)
+
+; enable eclim mode in all buffers
+(global-eclim-mode)
 
 ;; enable company-mode for completion
 (require 'company)
