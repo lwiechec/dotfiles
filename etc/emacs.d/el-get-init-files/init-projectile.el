09c65775be01cc5365abe8e33b32542c043dede9
Łukasz Wiecheć <lwiechec@o2.pl>
2013-10-23 10:03:59 +0200

    for some reason, helm-projectile is not autoloaded by default...
    this should take care of that

--- etc/emacs.d/el-get-init-files/init-projectile.el
+++ etc/emacs.d/el-get-init-files/init-projectile.el
@@ -1,2 +1,4 @@
 ;; turn on projectile mode globally
 (projectile-global-mode)
+;; autoload helm-projectile integration (for some reason, it doesn't load automatically)
+(autoload 'helm-projectile "helm-projectile" nil t)
