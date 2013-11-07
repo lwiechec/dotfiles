60beabc80d86500cd26b51e9d550758b0c274e8b
Łukasz Wiecheć <lwiechec@o2.pl>
2013-11-07 23:20:42 +0100

    better slime setup

--- etc/emacs.d/el-get-init-files/init-slime.el
+++ etc/emacs.d/el-get-init-files/init-slime.el
@@ -1,3 +1,10 @@
-(slime-setup '(slime-banner slime-fancy))
+(require 'slime-autoloads)
+
+(eval-after-load "slime"
+  '(progn
+     (slime-setup '(slime-banner slime-fancy))
+     (setq slime-complete-symbol*-fancy t)
+     (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)))
+
 ; i'm using sbcl for my lisp
 (setq inferior-lisp-program "sbcl")
