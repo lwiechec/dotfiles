11ed5716b8de2305f6c02f2f3a2f932a090093b9
Łukasz Wiecheć <lwiechec@o2.pl>
2013-11-19 10:23:50 +0100

    liking helm even more!

--- etc/emacs.d/el-get-init-files/init-helm.el
+++ etc/emacs.d/el-get-init-files/init-helm.el
@@ -1,4 +1,6 @@
 ;; I like helm-buffers-list better than Electric version so...
 (global-set-key (kbd "\C-x \C-b") 'helm-buffers-list)
+;; I also like Helm's version of find-files
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 ;; global helm mode
 (helm-mode 1)
