10ebbd39388d866b0b29c717fc3453fe854660d8
Łukasz Wiecheć <lwiechec@o2.pl>
2014-08-29 10:59:34 +0200

    adding journalling in org-mode

---
+++ etc/emacs.d/various/org-journal.el
@@ -0,0 +1,37 @@
+;;; package --- Summary
+
+;;; Commentary:
+;;; simple code to provide a way to write journal entries.Taken from Jack Moffit's blog: http://metajack.im/2009/01/01/journaling-with-emacs-orgmode/
+
+;;; Code:
+
+(require 'org-mode)
+
+(defvar org-journal-file "~/Dropbox/org/journal.org"
+  "Path to OrgMode journal file.")
+
+(defvar org-journal-date-format "%Y-%m-%d"
+  "Date format string for journal headings.")
+
+(defun org-journal-entry ()
+  "Create a new diary entry for today or append to an existing one.  Note that the entry for a day will be narrowed."
+  (interactive)
+  (switch-to-buffer (find-file org-journal-file))
+  (widen)
+  (let ((isearch-forward t) (today (format-time-string org-journal-date-format)))
+    (beginning-of-buffer)
+    (unless (org-goto-local-search-headings today nil t)
+      ((lambda ()
+         (org-insert-heading)
+         (insert today)
+         (insert "\n\n  \n"))))
+    (beginning-of-buffer)
+    (org-show-entry)
+    (org-narrow-to-subtree)
+    (end-of-buffer)
+    (backward-char 2)
+    (unless (= (current-column) 2)
+      (insert "\n\n  "))))
+
+(provide 'org-journal)
+;;; org-journal.el ends here
