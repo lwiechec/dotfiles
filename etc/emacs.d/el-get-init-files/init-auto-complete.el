fa0536b7ae1bc9fe5f293989983deb250ae9724a
Łukasz Wiecheć <lwiechec@o2.pl>
2014-11-22 23:44:02 +0100

    disabling auto-complete
    will use company-mode

--- etc/emacs.d/el-get-init-files/init-auto-complete.el
+++ etc/emacs.d/el-get-init-files/init-auto-complete.el
@@ -1,7 +1,7 @@
-(require 'auto-complete)
-(setq-default ac-sources (add-to-list 'ac-sources 'ac-source-dictionary))
-(global-auto-complete-mode t)
+;(require 'auto-complete)
+;(setq-default ac-sources (add-to-list 'ac-sources 'ac-source-dictionary))
+;(global-auto-complete-mode t)
 ; Start auto-completion after 2 characters of a word
-(setq ac-auto-start 2)
+;(setq ac-auto-start 2)
 ; case sensitivity is important when finding matches
-(setq ac-ignore-case nil)
+;(setq ac-ignore-case nil)
