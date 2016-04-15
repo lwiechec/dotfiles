ff7b48f3494e449e373ac491f4c27146a127a14b
Łukasz Wiecheć <lukasz@wiechec.eu>
2016-04-15 13:53:14 +0200

    removing unnecessary

--- etc/emacs.d/el-get-init-files/helm-ag.el
+++
@@ -1,6 +0,0 @@
-(require 'helm-ag)
-
-(custom-set-variables
- '(helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
- '(helm-ag-command-option "--all-text")
- '(helm-ag-insert-at-point 'symbol))
--- etc/emacs.d/el-get-init-files/init-skewer-mode.el
+++
@@ -1,3 +0,0 @@
-(add-hook 'js2-mode-hook 'skewer-mode)
-(add-hook 'css-mode-hook 'skewer-css-mode)
-(add-hook 'html-mode-hook 'skewer-html-mode)
