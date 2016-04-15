dcfa85ba27f37117463a4d45113664652df63e9b
Łukasz Wiecheć <lukasz@wiechec.eu>
2016-04-15 21:40:44 +0200

    fixing the modeline

--- etc/emacs.d/el-get-init-files/init-persp-projectile.el
+++ etc/emacs.d/el-get-init-files/init-persp-projectile.el
@@ -2,3 +2,6 @@
 (persp-mode 1)
 (require 'persp-projectile)
 (define-key projectile-mode-map (kbd "s-s") 'projectile-persp-switch-project)
+;; the persp takes too much space in the modeline, disabling
+(setq persp-show-modestring nil)
+(persp-turn-off-modestring)
