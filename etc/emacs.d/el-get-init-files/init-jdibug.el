4f5e7728590cb00d759241d5e5fe2f6eb403b90a
Łukasz Wiecheć <lukasz@wiechec.eu>
2016-04-16 14:11:57 +0200

    not using malabar anymore

--- etc/emacs.d/el-get-init-files/init-jdibug.el
+++ etc/emacs.d/el-get-init-files/init-jdibug.el
@@ -2,8 +2,8 @@
 (setq jdibug-connect-hosts '("localhost:4000" "localhost:5005"))
 
 ;; set the key bindings for my Java modes
-;(define-key malabar-mode-map (kbd "C-c d b") 'jdibug-breakpoints-toggle)
-;(define-key malabar-mode-map (kbd "C-c d n") 'jdibug-step-over)
-;(define-key malabar-mode-map (kbd "C-c d i") 'jdibug-step-into)
-;(define-key malabar-mode-map (kbd "C-c d o") 'jdibug-step-out)
-;(define-key malabar-mode-map (kbd "C-c d r") 'jdibug-resume)
+(define-key java-mode-map (kbd "C-c d b") 'jdibug-breakpoints-toggle)
+(define-key java-mode-map (kbd "C-c d n") 'jdibug-step-over)
+                                        (define-key java-mode-map (kbd "C-c d i") 'jdibug-step-into)
+(define-key java-mode-map (kbd "C-c d o") 'jdibug-step-out)
+(define-key java-mode-map (kbd "C-c d r") 'jdibug-resume)
