fd0d5329912d2da8bd2faefde5b9005c8c1cf2e0
Łukasz Wiecheć <lukasz@wiechec.eu>
2016-04-16 14:11:52 +0200

    fixing comment

--- etc/emacs.d/el-get-init-files/init-clojure-mode.el
+++ etc/emacs.d/el-get-init-files/init-clojure-mode.el
@@ -1,2 +1,3 @@
-; sometimes I have this strange error regardin this variable... this seems to fix it
+
+;; sometimes I have this strange error regardin this variable... this seems to fix it
 (defvar clojure--prettify-symbols-alist nil)
