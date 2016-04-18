734ef97e5a4552909c8ca5341c69cd993558e75e
Łukasz Wiecheć <lukasz@wiechec.eu>
2016-04-18 20:54:30 +0200

    some cleanups in the recipes

--- etc/emacs.d/el-get-init-files/init-jdibug.el
+++ etc/emacs.d/el-get-init-files/init-jdibug.el
@@ -4,6 +4,6 @@
 ;; set the key bindings for my Java modes
 (define-key java-mode-map (kbd "C-c d b") 'jdibug-breakpoints-toggle)
 (define-key java-mode-map (kbd "C-c d n") 'jdibug-step-over)
-                                        (define-key java-mode-map (kbd "C-c d i") 'jdibug-step-into)
+(define-key java-mode-map (kbd "C-c d i") 'jdibug-step-into)
 (define-key java-mode-map (kbd "C-c d o") 'jdibug-step-out)
 (define-key java-mode-map (kbd "C-c d r") 'jdibug-resume)
